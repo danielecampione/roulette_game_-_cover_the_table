8ddddd4675232ab121f23491b730305000730e6a
Daniele Campione <nonametekno@gmail.com>
2024-11-20 23:43:44 +0100

    0.01a
    0.01a

---
+++ src/Bet.java
@@ -0,0 +1,26 @@
+public class Bet {
+    private int bet1;
+    private int bet2;
+
+    public Bet(int bet1, int bet2) {
+        this.bet1 = bet1;
+        this.bet2 = bet2;
+    }
+
+    public boolean shouldIgnore() {
+        return bet1 == -1 && bet2 == -1;
+    }
+
+    public boolean isWinningNumber(int number) {
+        return number == bet1 || number == bet2;
+    }
+
+    @Override
+    public String toString() {
+        if (shouldIgnore()) {
+            return "ignora";
+        } else {
+            return bet1 + " " + bet2;
+        }
+    }
+}
---
+++ src/RouletteGame.java
@@ -0,0 +1,163 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class RouletteGame {
+	
+    public static void main(String[] args) {
+        Roulette roulette = new Roulette();
+        List<Bet> bets = new ArrayList<>();
+        bets.add(new Bet(17, 34));
+        bets.add(new Bet(2, 25));
+        bets.add(new Bet(4, 21));
+        bets.add(new Bet(6, 27));
+        bets.add(new Bet(13, 36));
+        bets.add(new Bet(-1, -1)); // Ignora
+        bets.add(new Bet(22, 18));
+        bets.add(new Bet(31, 9));
+        bets.add(new Bet(20, 14));
+        bets.add(new Bet(29, 7));
+        bets.add(new Bet(28, 12));
+        bets.add(new Bet(-1, -1)); // Ignora
+        bets.add(new Bet(17, 34));
+        bets.add(new Bet(2, 25));
+        bets.add(new Bet(4, 21));
+        bets.add(new Bet(6, 27));
+        bets.add(new Bet(13, 36));
+        bets.add(new Bet(-1, -1)); // Ignora
+        bets.add(new Bet(22, 18));
+        bets.add(new Bet(31, 9));
+        bets.add(new Bet(20, 14));
+        bets.add(new Bet(29, 7));
+        bets.add(new Bet(28, 12));
+        bets.add(new Bet(-1, -1)); // Ignora
+        bets.add(new Bet(17, 34));
+        bets.add(new Bet(2, 25));
+        bets.add(new Bet(4, 21));
+        bets.add(new Bet(6, 27));
+        bets.add(new Bet(13, 36));
+        bets.add(new Bet(-1, -1)); // Ignora
+        bets.add(new Bet(22, 18));
+        bets.add(new Bet(31, 9));
+        bets.add(new Bet(20, 14));
+        bets.add(new Bet(29, 7));
+        bets.add(new Bet(28, 12));
+        bets.add(new Bet(-1, -1)); // Ignora
+        bets.add(new Bet(17, 34));
+        bets.add(new Bet(2, 25));
+        bets.add(new Bet(4, 21));
+        bets.add(new Bet(6, 27));
+        bets.add(new Bet(13, 36));
+        bets.add(new Bet(-1, -1)); // Ignora
+        bets.add(new Bet(22, 18));
+        bets.add(new Bet(31, 9));
+        bets.add(new Bet(20, 14));
+        bets.add(new Bet(29, 7));
+        bets.add(new Bet(28, 12));
+        bets.add(new Bet(-1, -1)); // Ignora
+        bets.add(new Bet(17, 34));
+        bets.add(new Bet(2, 25));
+        bets.add(new Bet(4, 21));
+        bets.add(new Bet(6, 27));
+        bets.add(new Bet(13, 36));
+        bets.add(new Bet(-1, -1)); // Ignora
+        bets.add(new Bet(22, 18));
+        bets.add(new Bet(31, 9));
+        bets.add(new Bet(20, 14));
+        bets.add(new Bet(29, 7));
+        bets.add(new Bet(28, 12));
+        bets.add(new Bet(-1, -1)); // Ignora
+        bets.add(new Bet(17, 34));
+        bets.add(new Bet(2, 25));
+        bets.add(new Bet(4, 21));
+        bets.add(new Bet(6, 27));
+        bets.add(new Bet(13, 36));
+        bets.add(new Bet(-1, -1)); // Ignora
+        bets.add(new Bet(22, 18));
+        bets.add(new Bet(31, 9));
+        bets.add(new Bet(20, 14));
+        bets.add(new Bet(29, 7));
+        bets.add(new Bet(28, 12));
+        bets.add(new Bet(-1, -1)); // Ignora
+        bets.add(new Bet(17, 34));
+        bets.add(new Bet(2, 25));
+        bets.add(new Bet(4, 21));
+        bets.add(new Bet(6, 27));
+        bets.add(new Bet(13, 36));
+        bets.add(new Bet(-1, -1)); // Ignora
+        bets.add(new Bet(22, 18));
+        bets.add(new Bet(31, 9));
+        bets.add(new Bet(20, 14));
+        bets.add(new Bet(29, 7));
+        bets.add(new Bet(28, 12));
+        bets.add(new Bet(-1, -1)); // Ignora
+        bets.add(new Bet(17, 34));
+        bets.add(new Bet(2, 25));
+        bets.add(new Bet(4, 21));
+        bets.add(new Bet(6, 27));
+        bets.add(new Bet(13, 36));
+        bets.add(new Bet(-1, -1)); // Ignora
+        bets.add(new Bet(22, 18));
+        bets.add(new Bet(31, 9));
+        bets.add(new Bet(20, 14));
+        bets.add(new Bet(29, 7));
+        bets.add(new Bet(28, 12));
+        bets.add(new Bet(-1, -1)); // Ignora
+        bets.add(new Bet(17, 34));
+        bets.add(new Bet(2, 25));
+        bets.add(new Bet(4, 21));
+        bets.add(new Bet(6, 27));
+        bets.add(new Bet(13, 36));
+        bets.add(new Bet(-1, -1)); // Ignora
+        bets.add(new Bet(22, 18));
+        bets.add(new Bet(31, 9));
+        bets.add(new Bet(20, 14));
+        bets.add(new Bet(29, 7));
+        bets.add(new Bet(28, 12));
+        bets.add(new Bet(-1, -1)); // Ignora
+        bets.add(new Bet(17, 34));
+        bets.add(new Bet(2, 25));
+        bets.add(new Bet(4, 21));
+        bets.add(new Bet(6, 27));
+        bets.add(new Bet(13, 36));
+        bets.add(new Bet(-1, -1)); // Ignora
+        bets.add(new Bet(22, 18));
+        bets.add(new Bet(31, 9));
+        bets.add(new Bet(20, 14));
+        bets.add(new Bet(29, 7));
+        bets.add(new Bet(28, 12));
+        bets.add(new Bet(-1, -1)); // Ignora
+        bets.add(new Bet(17, 34));
+        bets.add(new Bet(2, 25));
+        bets.add(new Bet(4, 21));
+        bets.add(new Bet(6, 27));
+        bets.add(new Bet(13, 36));
+        bets.add(new Bet(-1, -1)); // Ignora
+        bets.add(new Bet(22, 18));
+        bets.add(new Bet(31, 9));
+        bets.add(new Bet(20, 14));
+        bets.add(new Bet(29, 7));
+        bets.add(new Bet(28, 12));
+        bets.add(new Bet(-1, -1)); // Ignora
+
+        boolean stopGame = false;
+        for (Bet bet : bets) {
+            if (bet.shouldIgnore()) {
+                System.out.println((stopGame ? "X " : ". ") + bet);
+                continue;
+            }
+
+            if (stopGame) {
+                System.out.println("X " + bet);
+                continue;
+            }
+
+            int result = roulette.spin();
+            if (bet.isWinningNumber(result)) {
+                System.out.println("X " + bet + " (" + result + ")");
+                stopGame = true;
+            } else {
+                System.out.println(". " + bet + " (" + result + ")");
+            }
+        }
+    }
+}
